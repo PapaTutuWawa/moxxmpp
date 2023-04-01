fbb495dc2f32360340212ac3ba8e66048c77fd19
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2023-04-01 23:16:37 +0200

    feat(xep): Allow inlining CSI

--- packages/moxxmpp/test/xeps/xep_0352_test.dart
+++ packages/moxxmpp/test/xeps/xep_0352_test.dart
@@ -1,5 +1,6 @@
 import 'package:moxxmpp/moxxmpp.dart';
 import 'package:test/test.dart';
+import '../helpers/logging.dart';
 import '../helpers/xmpp.dart';
 
 class MockedCSINegotiator extends CSINegotiator {
@@ -28,6 +29,8 @@ T? getUnsupportedCSINegotiator<T extends XmppFeatureNegotiatorBase>(String id) {
 }
 
 void main() {
+  initLogger();
+
   group('Test the XEP-0352 implementation', () {
     test('Test setting the CSI state when CSI is unsupported', () {
       var nonzaSent = false;
@@ -110,5 +113,96 @@ void main() {
         ..setActive()
         ..setInactive();
     });
+  });
+
+  test('Test CSI with Bind2', () async {
+    final fakeSocket = StubTCPSocket([
+      StringExpectation(
+        "<stream:stream xmlns='jabber:client' version='1.0' xmlns:stream='http://etherx.jabber.org/streams' to='test.server' xml:lang='en'>",
+        '''
+<stream:stream
+    xmlns="jabber:client"
+    version="1.0"
+    xmlns:stream="http://etherx.jabber.org/streams"
+    from="test.server"
+    xml:lang="en">
+  <stream:features xmlns="http://etherx.jabber.org/streams">
+    <mechanisms xmlns="urn:ietf:params:xml:ns:xmpp-sasl">
+      <mechanism>PLAIN</mechanism>
+    </mechanisms>
+    <authentication xmlns='urn:xmpp:sasl:2'>
+      <mechanism>PLAIN</mechanism>
+      <inline>
+        <bind xmlns="urn:xmpp:bind:0">
+          <inline>
+            <feature var="urn:xmpp:csi:0" />
+          </inline>
+        </bind>
+      </inline>
+    </authentication>
+    <bind xmlns="urn:ietf:params:xml:ns:xmpp-bind">
+      <required/>
+    </bind>
+  </stream:features>''',
+      ),
+      StanzaExpectation(
+        '''
+<authenticate xmlns='urn:xmpp:sasl:2' mechanism='PLAIN'>
+  <user-agent id='d4565fa7-4d72-4749-b3d3-740edbf87770'>
+    <software>moxxmpp</software>
+    <device>PapaTutuWawa's awesome device</device>
+  </user-agent>
+  <initial-response>AHBvbHlub21kaXZpc2lvbgBhYWFh</initial-response>
+  <bind xmlns='urn:xmpp:bind:0'>
+    <inactive xmlns='urn:xmpp:csi:0' />
+  </bind>
+</authenticate>''',
+        '''
+<success xmlns='urn:xmpp:sasl:2'>
+  <authorization-identifier>polynomdivision@test.server/test-resource</authorization-identifier>
+</success>
+        ''',
+      ),
+    ]);
+    final conn = XmppConnection(
+      TestingReconnectionPolicy(),
+      AlwaysConnectedConnectivityManager(),
+      fakeSocket,
+    )..setConnectionSettings(
+        ConnectionSettings(
+          jid: JID.fromString('polynomdivision@test.server'),
+          password: 'aaaa',
+          useDirectTLS: true,
+        ),
+      );
+    final csi = CSIManager();
+    await csi.setInactive(sendNonza: false);
+    await conn.registerManagers([
+      RosterManager(TestingRosterStateManager('', [])),
+      DiscoManager([]),
+      csi,
+    ]);
+    await conn.registerFeatureNegotiators([
+      SaslPlainNegotiator(),
+      ResourceBindingNegotiator(),
+      FASTSaslNegotiator(),
+      Bind2Negotiator(),
+      CSINegotiator(),
+      Sasl2Negotiator(
+        userAgent: const UserAgent(
+          id: 'd4565fa7-4d72-4749-b3d3-740edbf87770',
+          software: 'moxxmpp',
+          device: "PapaTutuWawa's awesome device",
+        ),
+      ),
+    ]);
+
+    final result = await conn.connect(
+      waitUntilLogin: true,
+      shouldReconnect: false,
+      enableReconnectOnSuccess: false,
+    );
+    expect(result.isType<NegotiatorError>(), false);
+    expect(fakeSocket.getState(), 2);
   });
 }
