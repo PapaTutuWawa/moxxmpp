64a8de6caabaa5d73f3ced1bcab2ccb6c7adc205
Ikjot Singh Dhody <ikjotsd@gmail.com>
2023-05-29 17:10:41 +0530

    feat(xep): Set base for XEP 0045 implementation
    Signed-off-by: Ikjot Singh Dhody <ikjotsd@gmail.com>

--- packages/moxxmpp/lib/src/managers/namespaces.dart
+++ packages/moxxmpp/lib/src/managers/namespaces.dart
@@ -31,4 +31,6 @@ const lastMessageCorrectionManager = 'org.moxxmpp.lastmessagecorrectionmanager';
 const messageReactionsManager = 'org.moxxmpp.messagereactionsmanager';
 const stickersManager = 'org.moxxmpp.stickersmanager';
 const entityCapabilitiesManager = 'org.moxxmpp.entitycapabilities';
+const messageProcessingHintManager = 'org.moxxmpp.messageprocessinghint';
+const occupantIdManager = 'org.moxxmpp.occupantidmanager';
 const mucManager = 'org.moxxmpp.mucmanager';
